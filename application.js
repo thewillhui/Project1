38bb1621c83f251bb47bf5a0242367ecd5a40c77
Will Hui <will@willhui.com>
2016-06-06 10:51:25 +0800

    initial commit

---
+++ application.js
@@ -0,0 +1,30 @@
+/*
+Pseudo code
+
+1. Create viewport
+  1.1. As a bonus it should be responsive
+2. Create global variables - sprites, background, current score, best score player 1, player 2
+3. Add spritesheet
+  3.1 Create variables for spritesheet
+4. Create gameloop - 60fps
+
+
+
+
+
+
+
+
+
+*/
+
+var character = $("#character");
+
+
+var gameLoop = setInterval(function() {
+  moveCharacter();
+}, 17);
+
+
+
+
