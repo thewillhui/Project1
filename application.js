6fbf766b47b5dbd3b5ef997126036c11c2f3cec9
Will Hui <will@willhui.com>
2016-06-07 23:40:25 +0800

    title screen and game start complete

--- application.js
+++ application.js
@@ -14,42 +14,64 @@ Pseudo code
     5.1.3. Stop blocks from moving when character dies
 6. Collision detection
   6.1 If character hit box hits the same position as a block then stop
+7. Title screen
+  7.1. Character floats waiting for user input. Game starts on user input
 */
 
 $(document).ready(function() {
-  // Invertal Variables
-  var gameLoop  = null;
+  // Interval Variables
+  var gameLoop = null;
+  var bounce = null;
 
   // HTML elements
-  var $character      = $("#character");
-  var $background     = $("#background");
+  var $character = $("#character");
+  var $background = $("#background");
+  var $ground = $("#ground");
   var $innerContainer = $("#innerContainer");
+  var $startScreen = $("#startScreen");
+  var $pressPlay = $("#pressPlay");
 
   // System Variable
-  var characterMovement           = 0;
-  var currentBlockGenerationSpeed = 1000;
-  var durationTillNextBlock       = currentBlockGenerationSpeed;
+  var characterMovement = 0;
+  var currentBlockGenerationSpeed = 700;
+  var durationTillNextBlock = currentBlockGenerationSpeed;
 
   // Default Settings
   var characterMovementIncrease = 20;
-  var keycode                   = 32; // Keycode for Spacebar
+  var keycode = 32; // Keycode for Spacebar
+  var started = false;
 
   // Game Settings
-  var characterHeight          = 72;
-  var characterWidth           = 69;
-  var characterInitialTop      = 30;
-  var characterInitialLeft     = 80;
-  var xMin                     = 0;
-  var xMax                     = 1000;
-  var yMin                     = 0;
-  var yMax                     = 600;
-  var gravityIncrease          = 6;
-  var blockWidth               = 80;
-  var blockSpeedDuration       = 3000;
-  var backgroundScrollDuration = 60000;
-  var gameLoopDuration         = 17;
+  var characterHeight = 72;
+  var characterWidth = 69;
+  var characterInitialTop = 250;
+  var characterInitialLeft = 80;
+  var xMin = 0;
+  var xMax = 1000;
+  var yMin = 0;
+  var yMax = 600;
+  var gravityIncrease = 6;
+  var blockWidth = 80;
+  var blockSpeedDuration = 2000;
 
-  var randomInt = function () {
+  var backgroundScrollDuration = 50000;
+  var groundScrollDuration = 20000;
+  var gameLoopDuration = 17;
+
+  //Scoring
+  var player1;
+  var player2;
+  var p1BestScore = 0;
+  var p2BestScore = 0;
+  var currScore = 0;
+
+  //Random height generator for the blocks
+  var randBlockHeight = function() {
+    return Math.floor((Math.random() * 230) + 150);
+  };
+
+  //Random interval generator for block generation frequency
+  var randomInt = function() {
     return Math.floor((Math.random() * 3000) + 500);
   };
 
@@ -60,13 +82,33 @@ $(document).ready(function() {
     $blockTop.stop();
     $blockBottom.stop();
     $background.stop(true);
+    $ground.stop(true);
     clearInterval(gameLoop);
   };
+
+  //game waits for user input before initiating
+  var gameWait = function() {
+    $character.css({ top: characterInitialTop, left: characterInitialLeft })
+    if (started === false) {
+      bounce = setInterval(function() {
+        $character.effect("bounce", { times: 1 }, 1000);
+        $pressPlay.effect("pulsate", { times: 0.5 }, 1000);
+      }, 1000);
+    }
+    scroll($ground, groundScrollDuration);
+    scroll($background, backgroundScrollDuration);
+  }
 
   $(document).on("keyup", function(e) {
     if (e.keyCode == keycode) {
       //how many times the character moves - multiplies the position.top below. higher value produces smoother movement but longer animations
       characterMovement += characterMovementIncrease;
+      if (started == false) {
+        started = true;
+        $character.stop();
+        clearInterval(bounce);
+        startGame();
+      }
     }
   });
 
@@ -101,57 +143,64 @@ $(document).ready(function() {
     }
   };
 
-  var createBlock = function (blockClass) {
+  var createBlock = function(blockClass) {
     var $newElem = $('<div></div>').addClass(blockClass);
+    var rBlockHeight = randBlockHeight();
     $innerContainer.append($newElem);
     $newElem.css({
-      left: xMax
+      left: xMax,
+      height: rBlockHeight
     }).animate({
       left: xMin - blockWidth
     }, {
       duration: blockSpeedDuration,
       easing: 'linear',
-      complete: function () {
+      complete: function() {
         $(this).remove();
       },
-      progress: function () {
+      progress: function() {
         var characterPos = $character.position();
-        var cLeft        = characterPos.left;
-        var cRight       = characterPos.left + characterWidth;
-        var cTop         = characterPos.top;
-        var cBot         = characterPos.top + characterHeight;
+        var cLeft = characterPos.left;
+        var cRight = characterPos.left + characterWidth;
+        var cTop = characterPos.top;
+        var cBot = characterPos.top + characterHeight;
 
-        var $block       = $(this);
-        var blockTopPos  = $block.position();
-        var blockHeight  = $block.height();
-        var bLeft        = blockTopPos.left;
-        var bTop         = blockTopPos.top;
-        var bBot         = blockTopPos.top + blockHeight;
+        var $block = $(this);
+        var blockPos = $block.position();
+        var blockHeight = $block.height();
+
+        var bLeft = blockPos.left;
+        var bRight = blockPos.left + blockWidth;
+        var bTop = blockPos.top;
+        var bBot = blockPos.top + blockHeight;
 
         // cLeft < bLeft < cRight
-        var horizontalCollision = cLeft <= bLeft && bLeft <= cRight;
+        var horizontalCollisionL = cLeft <= bLeft && bLeft <= cRight;
+        var horizontalCollisionR = cRight <= bRight && bLeft <= cRight;
 
         // bTop < cTop < bBot
         // bTop < cBot < bBot
-        var vertialCollisionTop = bTop <= cTop && cTop <= bBot ;
-        var vertialCollisionBot = bTop <= cBot && cBot <= bBot ;
+        var verticalCollisionTop = bTop <= cTop && cTop <= bBot;
+        var verticalCollisionBot = bTop <= cBot && cBot <= bBot;
 
-        if (horizontalCollision && vertialCollisionTop && vertialCollisionBot) {
+        if (horizontalCollisionL && horizontalCollisionR && verticalCollisionTop && verticalCollisionBot) {
           stopGame();
         }
       }
     });
   };
 
+
   var scroll = function(el, speed) {
     //scrolls the background, time controls the speed
-    el.animate({ "background-position": "-1000px" }, speed, 'linear', function() {
+    el.animate({ "background-position": "-" + 1000 + "px" }, speed, 'linear', function() {
+      //resets background back to 0
       el.css('background-position', '0');
       scroll(el, speed);
     });
   };
 
-  var blockGeneration = function () {
+  var blockGeneration = function() {
     durationTillNextBlock -= gameLoopDuration;
 
     if (durationTillNextBlock <= 0) {
@@ -162,23 +211,32 @@ $(document).ready(function() {
     }
   };
 
-  var startGame = function() {
-    $character.css({ top: characterInitialTop, left: characterInitialLeft });
+  // var score = function () {
+  // var blockPos = $newElem.position().left
+  //       //score +1 for passing through each block
+  //       if (blockPos <= 0 ) {
+  //         currScore += 1 ;
+  //       }
+  // console.log(currScore);
+  // };
 
+
+
+  var startGame = function() {
+    $startScreen.hide();
     //gameLoop loops functions that require to be run ~60 times per second
     gameLoop = setInterval(function() {
       var position = $character.position();
       if (position.top >= yMax - characterHeight) {
         stopGame();
-      } else {
+      } else if (started) {
         gravity();
         moveCharacter();
-        scroll($background, backgroundScrollDuration);
       }
-
       blockGeneration();
     }, gameLoopDuration);
+    // score();
   };
-
-  startGame();
+  //loop end
+  gameWait()
 });
